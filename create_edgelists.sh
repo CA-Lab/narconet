7b0cd86da83ebfeecba5065fffafcb55855ad36e
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-08-04 12:00:53 -0500

    batch scripts of book mining, plus data

---
+++ create_edgelists.sh
@@ -0,0 +1,7 @@
+python pickle2edgelist.py --pickle data/pickles/Los_Senores_del_Narco_AnabelHernandez.pickle --edgelist data/edgelists/Los_Senores_del_Narco_AnabelHernandez.tsv
+python pickle2edgelist.py --pickle data/pickles/El_imperio_del_Chapo_RodriguezCastaneda.pickle --edgelist data/edgelists/El_imperio_del_Chapo_RodriguezCastaneda.tsv
+python pickle2edgelist.py --pickle data/pickles/Narcoleaks_WilbertTorre.pickle --edgelist data/edgelists/Narcoleaks_WilbertTorre.tsv
+python pickle2edgelist.py --pickle data/pickles/Marca_de_sangre_HectorMauleonRodriguez.pickle --edgelist data/edgelists/Marca_de_sangre_HectorMauleonRodriguez.tsv
+python pickle2edgelist.py --pickle data/pickles/El_cartel_incomodo_JoseReveles.pickle --edgelist data/edgelists/El_cartel_incomodo_JoseReveles.tsv
+python pickle2edgelist.py --pickle data/pickles/El_cartel_de_Sinaloa_Diego_Enrique_Osorno.pickle --edgelist data/edgelists/El_cartel_de_Sinaloa_Diego_Enrique_Osorno.tsv
+python pickle2edgelist.py --pickle data/pickles/El_ultimo_narco_ChapoMalcolmBeith.pickle --edgelist data/edgelists/El_ultimo_narco_ChapoMalcolmBeith.tsv
---
+++ create_pickles.sh
@@ -0,0 +1,7 @@
+python txt2gpickle.py data/src/Los_Senores_del_Narco_AnabelHernandez.txt > data/pickles/Los_Senores_del_Narco_AnabelHernandez.pickle
+python txt2gpickle.py data/src/El_imperio_del_Chapo_RodriguezCastaneda.txt > data/pickles/El_imperio_del_Chapo_RodriguezCastaneda.pickle
+python txt2gpickle.py data/src/El_cartel_incomodo_JoseReveles.txt > data/pickles/El_cartel_incomodo_JoseReveles.pickle
+python txt2gpickle.py data/src/El_ultimo_narco_ChapoMalcolmBeith.txt > data/pickles/El_ultimo_narco_ChapoMalcolmBeith.pickle
+python txt2gpickle.py data/src/Narcoleaks_WilbertTorre.txt > data/pickles/Narcoleaks_WilbertTorre.pickle
+python txt2gpickle.py data/src/Marca_de_sangre_HectorMauleonRodriguez.txt > data/pickles/Marca_de_sangre_HectorMauleonRodriguez.pickle
+python txt2gpickle.py data/src/El_cartel_de_Sinaloa_Diego_Enrique_Osorno.txt > data/pickles/El_cartel_de_Sinaloa_Diego_Enrique_Osorno.pickle
